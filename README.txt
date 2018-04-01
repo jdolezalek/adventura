01cac30d2160ab4b09cde338d6fc3a353efa0b3f
dirzy <johny.dolezalek@gmail.com>
2018-04-01 13:19:00 +0200

    Fungující menu

---
+++ README.txt
@@ -0,0 +1,9 @@
+﻿ADVENTURA - PODIVNE PROBUZENI
+AUTOR: Jan Doležálek
+Verze: 1.00
+Jednoduchá textová adventura vytvořená jako seminární práce na VŠE.
+Úkolem hry je se dostat ze základního prostoru(hospoda) do finálního prostoru(doly).
+K tomu aby hru bylo možné dokončit je nutné promlouvat s osobami ve hře a tím si
+zpřístupnit další lokace. Hru je možné i prohrát pokud se hráč dostane do "prohrávací" lokace
+a pokud nemá potřebný předmět na překonání této lokace.
+Hru spouštíme z libovolného adresáře pomocí příkazu java -jar adventuraPP.jar
--- src/main/java/com/github/jdolezalek/adventura/ui/homeController.java
+++ src/main/java/com/github/jdolezalek/adventura/ui/homeController.java
@@ -3,8 +3,16 @@ package com.github.jdolezalek.adventura.ui;
 import java.util.Observable;
 import java.util.Observer;
 
+import com.github.jdolezalek.adventura.logika.Hra;
 import com.github.jdolezalek.adventura.logika.IHra;
 import com.github.jdolezalek.adventura.logika.Prostor;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -12,6 +20,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.control.MenuItem;
 
 public class homeController extends GridPane implements Observer {
 	
@@ -19,7 +28,19 @@ public class homeController extends GridPane implements Observer {
 	@FXML private TextArea textVypis;
 	@FXML private Button odesli;
 	@FXML private ListView<Prostor> seznamMistnosti;
+	@FXML private MenuItem menuItemEnd;
+	@FXML private MenuItem menuItemNewGame;
+	@FXML private MenuItem menuItemAbout;
+	@FXML private MenuItem menuItemHelp;
+	
 	private IHra hra;
+	
+	public static String readFile(String path, Charset encoding) 
+			  throws IOException 
+			{
+			  byte[] encoded = Files.readAllBytes(Paths.get(path));
+			  return new String(encoded, encoding);
+			}
 	
 	public void odesliPrikaz() {
 		String vstup = textVstup.getText();
@@ -43,9 +64,37 @@ public class homeController extends GridPane implements Observer {
 		hra.getHerniPlan().addObserver(this);
 		
 	}
+	
+	public void novaHra() {
+		
+		IHra hra = new Hra();
+		this.hra = hra;
+		seznamMistnosti.getItems().clear();
+		textVstup.setDisable(false);
+		odesli.setDisable(false);
+		inicializuj(hra);
+		
+	}
+	
+	public void konecHry() {
+		
+		textVypis.appendText("\n"+"Konec hry...");
+		textVstup.setDisable(true);
+		odesli.setDisable(true);
+	}
+	
+	public void oHre() throws IOException {
+		textVypis.appendText("\n"+"\n"+readFile("README.txt", StandardCharsets.UTF_8));
+	}
+	
+	public void napoveda() throws IOException, URISyntaxException
+	{
+		java.awt.Desktop.getDesktop().browse(new java.net.URI("Uzivatelska_prirucka.html"));
+	}
 
 	@Override
 	public void update(Observable o, Object arg) {
+		seznamMistnosti.getItems().clear();
 		seznamMistnosti.getItems().addAll(hra.getHerniPlan().getAktualniProstor().getVychody());
 		
 	}
